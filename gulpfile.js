35789c47d0b9edecff60feb6d0f430eb20e13738
akshbhu <39866697+akshbhu@users.noreply.github.com>
2024-03-01 06:06:33 -0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -84,7 +84,7 @@ gulp.task('browsersync', function(callback) {
     server: {
       baseDir: [paths.dist.base.dir, paths.src.base.dir, paths.base.base.dir]
     },
-    open : false
+    open: false
   });
   callback();
 });
@@ -226,4 +226,4 @@ gulp.task('html', function() {
 
 gulp.task('build', gulp.series(gulp.parallel('clean:packageLock', 'clean:dist', 'copy:all', 'copy:libs'), 'fileinclude', 'js', 'scss', 'html'));
 
-gulp.task('default', gulp.series(gulp.series(gulp.parallel('clean:packageLock', 'clean:dist', 'copy:all', 'copy:libs'), 'fileinclude', 'js', 'scss',  'html'), gulp.parallel('browsersync', 'watch')));
+gulp.task('default', gulp.series(gulp.parallel('clean:packageLock', 'clean:dist', 'copy:all', 'copy:libs', 'fileinclude', 'js', 'scss',  'html'), gulp.parallel('browsersync', 'watch')));
